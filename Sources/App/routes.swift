e871a1c26b407e866f08a22dafe62eb7b90e48d9
Petr Brazdil <p.brazdil@gmail.com>
2018-12-06 13:40:18 +0100

    Include bundle

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,16 +7,19 @@ public func routes(_ router: Router) throws {
         return "Remote Config Service"
     }
     
-    router.get("ios", String.parameter) { req -> AppConfig in
+    router.get("ios", String.parameter, String.parameter) { req -> AppConfig in
         var content: [String: String] = [:]
         content["TARGET_EPC"] = "2.29"
         content["MODE"] = "all"
         
+        let bundle = try req.parameters.next(String.self)
         let version = try req.parameters.next(String.self)
-        switch version {
-        case "0.25.26":
-            content["MODE"] = "basic"
-        default: break
+        if bundle == "com.electrious.Management" {
+            switch version {
+            case "0.25.26":
+                content["MODE"] = "basic"
+            default: break
+            }
         }
         
         return AppConfig(
